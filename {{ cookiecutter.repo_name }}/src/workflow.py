edf4eec34b87670666fd2e0a8b9d60a292991d7b
Austin.Madert <Austin.Madert@kiewit.com>
2020-05-26 12:18:10 -0500

    Removed string jinja templating from workflow.py to debug cookiecutter build error

--- {{ cookiecutter.repo_name }}/src/workflow.py
+++ {{ cookiecutter.repo_name }}/src/workflow.py
@@ -62,9 +62,9 @@ t2 = operators.BashOperator(
 
 templated_command = "Turn the below into a string for use."
 {% for i in range(5) %}
-    echo "{{ ds }}"
-    echo "{{ macros.ds_add(ds, 7)}}"
-    echo "{{ params.my_param }}"
+    echo {{ ds }}
+    echo {{ macros.ds_add(ds, 7)}}
+    echo {{ params.my_param }}
 {% endfor %}
 
 
